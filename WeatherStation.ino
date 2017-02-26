b79110e2272fb4388c9cd01132f96009c955b92f
vls29 <git@v-s-f.co.uk>
2017-02-26 16:10:26 +0000

    Format + Skip Ethernet
    Add in a skip ethernet constant so that it's possible to test out the
    sketch on the board without connecting to the ethernet.
    
    Format the code

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -18,7 +18,9 @@ const int pressureSensorPin = A2;
 const int anemometerSensorPin = A3;
 
 const bool trace = false;
-const bool debug = false;
+const bool debug = true;
+
+const bool SKIP_ETHERNET_INIT = true;
 
 ///////// CHANGEABLE VALUES ABOVE /////////
 
@@ -34,13 +36,15 @@ const int ZERO_INT = 0;
 const double PRESSURE_TEMPERATURE_ERROR_FACTOR_NORMAL = 1.0;
 const double KPA_TO_MILLIBARS = 10.0;
 
+long loopCounter = 0L;
+
 double temperatureReadings = ZERO_DOUBLE;
 double humidityReadings = ZERO_DOUBLE;
 double pressureReadings = ZERO_DOUBLE;
 
 
 EthernetClient pompeiiClient;
-const byte mac[] = {0x90, 0xA0, 0xDA, 0x0E, 0x9B, 0xE6};
+byte mac[] = {0x90, 0xA0, 0xDA, 0x0E, 0x9B, 0xE6};
 char pompeiiService[] = "/pvoutput-post-weather.php";
 
 unsigned int windCircuitOnCount = ZERO_INT;
@@ -65,7 +69,9 @@ unsigned long gustPeriod = 1000UL;
 
 void setup() {
   Serial.begin(9600);
-  connectToEthernet();
+  if (!SKIP_ETHERNET_INIT) {
+    connectToEthernet();
+  }
 }
 
 void connectToEthernet() {
@@ -82,7 +88,7 @@ void connectToEthernet() {
 
       if (Ethernet.begin(mac) == 0) {
         Serial.println("Failed to configure Ethernet using DHCP stopping - will need reset");
-        while(true);
+        while (true);
       }
     }
 
@@ -108,7 +114,7 @@ void loop()
 
   if (isTimeToUploadData()) {
     Serial.println("Uploading data");
-    if(debug) {
+    if (debug) {
       Serial.println("******************************************************");
     }
     double averageTemperature = calculateAverageTemperature();
@@ -116,7 +122,7 @@ void loop()
     double averageTemperatureCompensatedPressure = calculateAverageTemperatureCompensatedPressure(averageTemperature);
     double windSpeed = getWindSpeedInMph();
     double gustSpeed = getGustSpeedInMph();
-    if(debug) {
+    if (debug) {
       Serial.println("******************************************************");
     }
 
@@ -130,7 +136,7 @@ void resetFlags() {
   windCircuitOnCount = ZERO_INT;
   windCircuitOnCountGust = ZERO_INT;
   gustMaxCount = ZERO_INT;
-  
+
   resetAverageTemperature();
   resetAverageHumidity();
   resetAveragePressure();
@@ -141,13 +147,13 @@ void resetFlags() {
 boolean isTimeToUploadData() {
   unsigned long currentTime = millis();
 
-  if(currentTime < previousTime)  {
+  if (currentTime < previousTime)  {
     lastTimeUploaded = currentTime;
   }
 
   previousTime = currentTime;
 
-  if( (currentTime - lastTimeUploaded) >= millisecondsBetweenCalls) {
+  if ( (currentTime - lastTimeUploaded) >= millisecondsBetweenCalls) {
     Serial.println("Time to upload");
     lastTimeUploaded = currentTime;
     return true;
@@ -162,19 +168,19 @@ boolean isTimeToUploadData() {
 double read5VAnalogPin(const int pin) {
   double sensorVal = analogRead(pin);
 
-  if(trace) {
+  if (trace) {
     Serial.print("Raw pin '");
     Serial.print(pin);
-  Serial.print("' sensor value: ");
+    Serial.print("' sensor value: ");
     Serial.println(sensorVal);
   }
-  
+
   sensorVal = sensorVal * (FIVE_VOLTS / ANALOG_PIN_RANGE);
-  
-  if(trace) {
+
+  if (trace) {
     Serial.print("Pin '");
     Serial.print(pin);
-  Serial.print("' true value: ");
+    Serial.print("' true value: ");
     Serial.println(sensorVal);
   }
 
@@ -186,11 +192,11 @@ void incrementLoopCounter() {
 }
 
 void resetLoopCounter() {
-  if(debug) {
-      Serial.print("Loops completed ");
-      Serial.println(loopCounter);
+  if (debug) {
+    Serial.print("Loops completed ");
+    Serial.println(loopCounter);
   }
-  
+
   loopCounter = ZERO_LONG;
 }
 
@@ -199,11 +205,11 @@ void resetLoopCounter() {
 ///////////////////////////////////////////////////////
 
 String generatePostData(double averageTemperature, double averageTemperatureCompensatedHumidity, double averageTemperatureCompensatedPressure, double windSpeed, double gustSpeed) {
-  return "temperature=" + String(averageTemperature) + 
-    "&humidity=" + String(averageTemperatureCompensatedHumidity) +
-    "&pressure=" + String(averageTemperatureCompensatedPressure) +
-    "&windSpeed=" + String(windSpeed) +
-    "&gustSpeed=" + String(gustSpeed);
+  return "temperature=" + String(averageTemperature) +
+         "&humidity=" + String(averageTemperatureCompensatedHumidity) +
+         "&pressure=" + String(averageTemperatureCompensatedPressure) +
+         "&windSpeed=" + String(windSpeed) +
+         "&gustSpeed=" + String(gustSpeed);
 }
 
 void sendResultsToPompeii(double averageTemperature, double averageTemperatureCompensatedHumidity, double averageTemperatureCompensatedPressure, double windSpeed, double gustSpeed) {
@@ -212,32 +218,34 @@ void sendResultsToPompeii(double averageTemperature, double averageTemperatureCo
   String postData = generatePostData(averageTemperature, averageTemperatureCompensatedHumidity, averageTemperatureCompensatedPressure, windSpeed, gustSpeed);
   Serial.println("post data: " + postData);
 
-  if (pompeiiClient.connect(pompeii, pompeiiPort)) {
-    Serial.println("connected to pompeii");
-    // Make a HTTP request:
-    pompeiiClient.print("POST ");
-    pompeiiClient.print(pompeiiService);
-    pompeiiClient.println(" HTTP/1.1");
-    pompeiiClient.print("Host: ");
-    pompeiiClient.print(pompeii);
-    pompeiiClient.print(":");
-    pompeiiClient.println(pompeiiPort);
-    pompeiiClient.println("Accept: text/html");
-    pompeiiClient.println("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
-    pompeiiClient.print("Content-Length: ");
-    pompeiiClient.println(postData.length());
-    pompeiiClient.println("Pragma: no-cache");
-    pompeiiClient.println("Cache-Control: no-cache");
-    pompeiiClient.println("Connection: close");
-    pompeiiClient.println("X-Data-Source: WEATHER");
-    pompeiiClient.println();
+  if (!SKIP_ETHERNET_INIT) {
+    if (pompeiiClient.connect(pompeii, pompeiiPort)) {
+      Serial.println("connected to pompeii");
+      // Make a HTTP request:
+      pompeiiClient.print("POST ");
+      pompeiiClient.print(pompeiiService);
+      pompeiiClient.println(" HTTP/1.1");
+      pompeiiClient.print("Host: ");
+      pompeiiClient.print(pompeii);
+      pompeiiClient.print(":");
+      pompeiiClient.println(pompeiiPort);
+      pompeiiClient.println("Accept: text/html");
+      pompeiiClient.println("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
+      pompeiiClient.print("Content-Length: ");
+      pompeiiClient.println(postData.length());
+      pompeiiClient.println("Pragma: no-cache");
+      pompeiiClient.println("Cache-Control: no-cache");
+      pompeiiClient.println("Connection: close");
+      pompeiiClient.println("X-Data-Source: WEATHER");
+      pompeiiClient.println();
 
-    pompeiiClient.println(postData);
-    pompeiiClient.println();
+      pompeiiClient.println(postData);
+      pompeiiClient.println();
 
-    pompeiiClient.stop();
-    pompeiiClient.flush();
-    Serial.println("Called pompeii");
+      pompeiiClient.stop();
+      pompeiiClient.flush();
+      Serial.println("Called pompeii");
+    }
   }
 }
 
@@ -246,14 +254,14 @@ void sendResultsToPompeii(double averageTemperature, double averageTemperatureCo
 ///////////////////////////////////////////////////////
 
 void readInstantAnemometerSensorValue() {
-  int sensorVal = analogRead(sensorPin);
+  int sensorVal = analogRead(anemometerSensorPin);
 
-  if( sensorVal > anemometerOnState) {
-    if( !lastStatus ) {
+  if ( sensorVal > anemometerOnState) {
+    if ( !lastStatus ) {
       windCircuitOnCount++;
       lastStatus = true;
     }
-  } 
+  }
   else {
     lastStatus = false;
   }
@@ -263,42 +271,42 @@ void calculateGustSpeed() {
   unsigned long currentTime = millis();
 
   // deals with the clock starting from zero after overflowing
-  if(currentTime < previousGustTime)  {
+  if (currentTime < previousGustTime)  {
     lastTimeGustCalculated = currentTime;
   }
 
   previousGustTime = currentTime;
 
-  if( (currentTime - lastTimeGustCalculated) >= gustPeriod) {
+  if ( (currentTime - lastTimeGustCalculated) >= gustPeriod) {
     //Serial.println(currentTime - lastTimeGustCalculated);
-    
+
     // calc the difference between the count on and the last value gust count was and if the count is greater than the last gust reading, store it.
     int countDifferenceSinceLastCalc = windCircuitOnCount - windCircuitOnCountGust;
-    
+
     //Serial.print("Gust: ");
     //Serial.println(countDifferenceSinceLastCalc);
     //Serial.print("CurrentMax: ");
     //Serial.println(gustMaxCount);
-    
-    if( countDifferenceSinceLastCalc > gustMaxCount ) {
+
+    if ( countDifferenceSinceLastCalc > gustMaxCount ) {
       gustMaxCount = countDifferenceSinceLastCalc;
     }
 
     // store the current wind on count so that the calculation for the gust is using the last value that was previously checked against.
     windCircuitOnCountGust = windCircuitOnCount;
-    
+
     lastTimeGustCalculated = currentTime;
   }
 }
 
 double getWindSpeedInMph() {
-  if(windCircuitOnCount == ZERO_INT) {
+  if (windCircuitOnCount == ZERO_INT) {
     return ZERO_DOUBLE;
   }
-  
+
   double mph = ((double)windCircuitOnCount / SECONDS_PER_MINUTE) * ANEMOMETER_CIRCUMFERENCE * MPS_TO_MPH;
-  
-  if(debug) {
+
+  if (debug) {
     Serial.print("Wind mph: ");
     Serial.println(mph);
   }
@@ -307,13 +315,13 @@ double getWindSpeedInMph() {
 }
 
 double getGustSpeedInMph() {
-  if(gustMaxCount == ZERO_INT) {
+  if (gustMaxCount == ZERO_INT) {
     return ZERO_DOUBLE;
   }
 
   double mph = ((double)gustMaxCount / SECONDS_PER_MINUTE) * ANEMOMETER_CIRCUMFERENCE * MPS_TO_MPH;
 
-  if(debug) {
+  if (debug) {
     Serial.print("Gust mph: ");
     Serial.println(mph);
   }
@@ -331,7 +339,7 @@ double getGustSpeedInMph() {
  */
 void readInstantHumiditySensorValue() {
   double humidity = read5VAnalogPin(humiditySensorPin);
-  if(trace) {
+  if (trace) {
     Serial.print("Humidity sensor value: ");
     Serial.println(humidity);
   }
@@ -344,16 +352,16 @@ void readInstantHumiditySensorValue() {
  * All the calculations are done here for efficiency.
  */
 double calculateAverageTemperatureCompensatedHumidity(double averageTemperature) {
-  double averageNonCompensatedHumidity = (double)humidityReadings/(double)loopCounter;
-  
+  double averageNonCompensatedHumidity = (double)humidityReadings / (double)loopCounter;
+
   // From Honeywell: Vout = (Vsupply)(0.00636(Sensor RH) + 0.1515), typical at 25 deg. C
   // Or written to calculate Relative Humidity: Relative Humidity = ((Vout / Vsupply) - 0.1515) / 0.00636, typical at 25 deg. C
   double averageRelativeHumidity = ((averageNonCompensatedHumidity / FIVE_VOLTS) - 0.1515) / 0.00636;
 
   // Temperature Compensated Relative Humidity = (Relative Humidity)/(1.0546 - 0.00216T), T in deg. C
   double averageTemperatureCompensatedHumidity = (averageRelativeHumidity / (1.0546 - (0.00216 * averageTemperature)));
-  
-  if(debug) {
+
+  if (debug) {
     Serial.print("Average humidity sensor value: ");
     Serial.println(averageNonCompensatedHumidity);
     Serial.print("Average non-compensated humidity: ");
@@ -361,7 +369,7 @@ double calculateAverageTemperatureCompensatedHumidity(double averageTemperature)
     Serial.print("Temperature compensated humidity: ");
     Serial.println(averageTemperatureCompensatedHumidity);
   }
-  
+
   return averageTemperatureCompensatedHumidity;
 }
 
@@ -375,12 +383,12 @@ void resetAverageHumidity() {
 
 void readInstantTemperatureSensorValue() {
   double temperature = calculateInstantTemperature();
-  
-  if(trace) {
+
+  if (trace) {
     Serial.print("temperature: ");
     Serial.println(temperature);
   }
-  
+
   temperatureReadings = temperatureReadings + temperature;
 }
 
@@ -392,9 +400,9 @@ double calculateInstantTemperature() {
 }
 
 double calculateAverageTemperature() {
-  double averageTemperature = temperatureReadings/(double)loopCounter;
+  double averageTemperature = temperatureReadings / (double)loopCounter;
 
-  if(debug) {
+  if (debug) {
     Serial.print("Average Temperature: ");
     Serial.println(averageTemperature);
   }
@@ -414,11 +422,11 @@ void resetAverageTemperature() {
 void readInstantPressureSensorValue() {
   double uncompensatedPressure = read5VAnalogPin(pressureSensorPin);
 
-  if(trace) {
+  if (trace) {
     Serial.print("Uncompensated pressure: ");
     Serial.println(uncompensatedPressure);
   }
-  
+
   pressureReadings = pressureReadings + uncompensatedPressure;
 }
 
@@ -431,13 +439,13 @@ void readInstantPressureSensorValue() {
  * NOT USING the error factor as it's too difficult to factor in as it can be positive or negative...
  */
 double calculateAverageTemperatureCompensatedPressure(double averageTemperature) {
-  double averagePressure = (double)pressureReadings/(double)loopCounter;
+  double averagePressure = (double)pressureReadings / (double)loopCounter;
 
   double calculatedPressure = ((averagePressure / FIVE_VOLTS) + 0.04) / 0.004;
   //pressure = (pressure - getErrorFactor(instantTemperature)) * kpaToMillibarsMultiplier;
   calculatedPressure = calculatedPressure * KPA_TO_MILLIBARS;
-  
-  if(debug) {
+
+  if (debug) {
     Serial.print("Average pressure sensor reading: ");
     Serial.println(averagePressure);
     Serial.print("Calculated pressure: ");
@@ -466,18 +474,20 @@ void resetAveragePressure() {
  */
 double getErrorFactor(double instantTemperature) {
   double temperatureCompensation = PRESSURE_TEMPERATURE_ERROR_FACTOR_NORMAL;
-  if( instantTemperature < 0.0 || instantTemperature > 85.0)
+  if ( instantTemperature < 0.0 || instantTemperature > 85.0)
   {
     double difference = 0.0;
-    if(instantTemperature > 0.0)  {
-        difference = instantTemperature - 85.0;
+    if (instantTemperature > 0.0)  {
+      difference = instantTemperature - 85.0;
     } else {
       // invert the negative number
       difference = instantTemperature - (instantTemperature * 2.0);
     }
 
-    temperatureCompensation = (difference * (2.0/40.0)) + 1.0;
+    temperatureCompensation = (difference * (2.0 / 40.0)) + 1.0;
   }
 
   return (3.45 * temperatureCompensation * 0.004 * FIVE_VOLTS);
 }
+
+
